58390a2b50e68078d7d23c5414d3d7f691fdb0a7
EmilRamsvik <54535686+EmilRamsvik@users.noreply.github.com>
2019-10-30 09:31:04 +0100

    Fix up
    Removed useless variables and changed documentation and cleaned up the code to match style guide used. Documented the function but the test class should also be documented but as only one public function the documentation is post-poned.

--- test_collection.py
+++ test_collection.py
@@ -11,44 +11,45 @@ Project: One operation (ONEOPS)
 
 Purpose: test function for TSI Client package
 
-Description: Makes test for unit testing the functions for TSI Client
-    
-Parameters: 
+Description: Makes test for unit testing the functions for TSI Client package
+
+Tests: 
+    test_function_works (function) : Function that calls on the TSIClient and
+        implements a class instance and asserts that the instance is created.
 
 Output: ok or test failed
 
+TODO: 
+    -Create a mock class and test fucntions that asserts that the function 
+        calls on the API  and get a response. 
 @author: Emil.Ramsvik
 @ Email: Emil.Ramsvik@wilhelmsen.com
 """
 import pytest
 from TSIClient import TSIClient as tsi
 
-
-
 class TestClass():
-    name = 'FakeName'
+
     def test_function_works(self):
         """Check that the client class can initate """
-        client = createTSIClient()
+        client = create_TSIClient()
         assert client  
-
         
-"""        
-Created on Wed Oct  2 15:54:01 2019
 
-- Functions used in testing environment to initate functions in 
-    TSIClient. 
-
-
-@author: Siri.Ovregard
-"""
-
-
-
-def createTSIClient():
-    tsiKeys = tsi.TSIClient(enviroment = 'Test Environment',
-                         client_id = "MyClientID",
-                         client_secret = "averysecretpassword",
-                         applicationName = "postmanServicePrincipal",
-                         tenant_id="yeatanothertenantid")
-    return tsiKeys
+def create_TSIClient():
+    """        
+    Version: 0.3
+    Created on Wed Oct  2 15:54:01 2019
+    - Function that creates and instant of the TSIClient class using dummmy input.
+    
+    Output:
+        tsi_keys (class instance) : A class instance with dummy variables
+    @author: Siri.Ovregard
+    Amended by Emil Ramsvik for use in unit test for TSIClient
+    """
+    tsi_keys = tsi.TSIClient(enviroment='Test Environment',
+                         client_id="MyClientID",
+                         client_secret="a_very_secret_password",
+                         applicationName="postmanServicePrincipal",
+                         tenant_id="yet_another_tenant_id")
+    return tsi_keys
