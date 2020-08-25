321ef2b8fa1ac8e6eed44b9af70b6d7954ceb5f5
Maiken Berthelsen <maikenberthelsen@Maikens-MacBook-Pro-2.local>
2020-08-25 13:22:00 +0200

    Environment api to get env_id and env_availability

--- TSIClient/environment/environment_api.py
+++ TSIClient/environment/environment_api.py
@@ -15,12 +15,12 @@ class EnvironmentApi:
         common_funcs: CommonFuncs,
     ):
         self._applicationName = application_name
-        self._enviromentName = environment
+        self._environmentName = environment
         self.authorization_api = authorization_api
         self.common_funcs = common_funcs
 
 
-    def getEnviroment(self):
+    def getEnvironmentId(self):
         """Gets the id of the environment specified in the TSIClient class constructor.
 
         Returns:
@@ -32,7 +32,7 @@ class EnvironmentApi:
         Example:
             >>> from TSIClient import TSIClient as tsi
             >>> client = tsi.TSIClient()
-            >>> env = client.getEnviroment()
+            >>> env = client.getEnvironment()
         """
 
         authorizationToken = self.authorization_api._getToken()
@@ -69,9 +69,9 @@ class EnvironmentApi:
 
         environments = json.loads(response.text)["environments"]
         environmentId = None
-        for enviroment in environments:
-            if enviroment["displayName"] == self._enviromentName:
-                environmentId = enviroment["environmentId"]
+        for environment in environments:
+            if environment["displayName"] == self._environmentName:
+                environmentId = environment["environmentId"]
                 break
         if environmentId == None:
             raise TSIEnvironmentError(
@@ -79,3 +79,48 @@ class EnvironmentApi:
             )
 
         return environmentId
+
+    def getEnvironmentAvailability(self):
+        """Returns the time range and distribution of event count over the event timestamp.
+        Can be used to provide landing experience of navigating to the environment.
+
+        Returns:
+            dict: The environment availability. Contains interval size, distribution and range.
+
+        Example:
+            >>> from TSIClient import TSIClient as tsi
+            >>> client = tsi.TSIClient()
+            >>> env_availability = client.getEnvironmentAvailability()
+        """
+
+        environmentId = self.getEnvironmentId()
+        authorizationToken = self.authorization_api._getToken()
+        url = "https://{environmentId}.env.timeseries.azure.com/availability".format(
+            environmentId=environmentId,
+        )
+        querystring = self.common_funcs._getQueryString()
+        payload = ""
+        headers = {
+            'x-ms-client-application-name': self._applicationName,
+            'Authorization': authorizationToken,
+            'Content-Type': "application/json",
+            'cache-control': "no-cache"
+        }
+        try:
+            response = requests.request(
+                "GET",
+                url,
+                data=payload,
+                headers=headers,
+                params=querystring,
+                timeout=10
+            )
+            response.raise_for_status()
+        except requests.exceptions.ConnectTimeout:
+            logging.error("TSIClient: The request to the TSI api timed out.")
+            raise
+        except requests.exceptions.HTTPError:
+            logging.error("TSIClient: The request to the TSI api returned an unsuccessfull status code.")
+            raise
+
+        return json.loads(response.text)
