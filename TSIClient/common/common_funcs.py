2c1dec0085fc51d938a304d5b5383070cbb36178
Maiken Berthelsen <maikenberthelsen@Maikens-MacBook-Pro-2.local>
2020-08-27 10:25:59 +0200

    added updateTimeseries to common_funcs

--- TSIClient/common/common_funcs.py
+++ TSIClient/common/common_funcs.py
@@ -1,3 +1,6 @@
+import json
+import requests
+
 class CommonFuncs:
     def __init__(self, api_version):
         self.api_version = api_version
@@ -24,3 +27,31 @@ class CommonFuncs:
                 "api-version": self.api_version,
                 "storeType": "WarmStore" if useWarmStore == True else "ColdStore",
             }
+    def _updateTimeSeries(self, payload, timeseries, applicationName, environmentId, authorizationToken):
+        """Writes instances to the TSI environment.
+
+        Args:
+            payload (str): A json-serializable payload that is posted to the TSI environment.
+                The format of the payload is specified in the Azure TSI documentation.
+
+        Returns:
+            dict: The response of the TSI api call.
+        """
+
+        url = "https://{environmentId}.env.timeseries.azure.com/timeseries/{timeseries}/$batch".format(environmentId=environmentId,timeseries=timeseries)
+        
+        querystring = self._getQueryString()
+
+        headers = {
+            'x-ms-client-application-name': applicationName,
+            'Authorization': authorizationToken,
+            'Content-Type': "application/json",
+            'cache-control': "no-cache"
+        }
+
+        response = requests.request("POST", url, data=json.dumps(payload), headers=headers, params=querystring)
+
+        if response.text:
+            jsonResponse = json.loads(response.text)
+
+        return jsonResponse
