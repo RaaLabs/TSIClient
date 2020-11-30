8dc2029c393b9a48cf23fb61dd38e182c243131d
Rafael Schlatter <rafaelschlatter@gmail.com>
2020-11-30 09:41:28 +0100

    Pypi friendly setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,32 @@
 from setuptools import setup, find_packages
 
 
-with open('README.md') as f:
+with open("README.md") as f:
     long_description = f.read()
 
 setup(
-  name = 'TSIClient',
-  packages = find_packages(),
-  version = '2.0.0',
-  license='MIT',
-  long_description=long_description,
-  long_description_content_type='text/markdown',
-  author = 'Raa Labs',
-  author_email = 'gill@outlook.com',
-  url = 'https://github.com/RaaLabs/TSIClient',
-  #download_url = 'https://github.com/RaaLabs/TSIClient/archive/v_0.7.tar.gz',    # I explain this later on
-  keywords = ['Time Series Insight', 'TSI', 'TSI SDK'],
-  install_requires=[
-          'requests',
-          'pandas'
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3.6',
-  ],
+    name="TSIClient",
+    packages=find_packages(),
+    version="2.0.0",
+    license="MIT",
+    description="The TSIClient is a Python SDK for Microsoft Azure time series insights.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Raa Labs",
+    author_email="gill@outlook.com",
+    url="https://github.com/RaaLabs/TSIClient",
+    project_urls={
+        "Bug Tracker": "https://github.com/RaaLabs/TSIClient/issues",
+        "Documentation": "https://raalabs-tsiclient.readthedocs.io/en/latest/",
+        "Source Code": "https://github.com/RaaLabs/TSIClient",
+    },
+    keywords=["Time Series Insights", "TSI", "TSI SDK", "Raa Labs", "IoT"],
+    install_requires=["requests", "pandas"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+    ],
 )
