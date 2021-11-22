ec2a5d4bc42482e757e08422be148d21ac7d42e5
Rafael Schlatter <rafaelschlatter@gmail.com>
2021-11-22 13:43:04 +0100

    Fix image link in conf.py for sphinx docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,7 +55,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = "RAA_labs_logo_greyscale.png"
+html_logo = "raalabs-logo-blue-yellow-RGB.png"
 html_theme_options = {
     'logo_only': False,
     'display_version': True,
